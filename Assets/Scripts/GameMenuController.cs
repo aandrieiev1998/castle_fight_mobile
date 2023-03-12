3f23586afcfb11d6878ce64c0d407a4d65c14b5a
Cr1pex <maxintuziuk@gmail.com>
2023-03-12 04:51:00 +0200

    sssssssssssssssssssss

---
+++ Assets/Scripts/GameMenuController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class GameMenuController : MonoBehaviour
+    {
+        [SerializeField] private GameObject overlay;
+        [SerializeField] private Button menuButton;
+        [SerializeField] private Button unpauseButton;
+
+        private void Start()
+        {
+            menuButton.onClick.AddListener(MenuButtonOnClick);
+            unpauseButton.onClick.AddListener(UnpauseButtonOnClick);
+        }
+
+        private void MenuButtonOnClick()
+        {
+            overlay.SetActive(!overlay.activeSelf);
+        }
+
+        private void UnpauseButtonOnClick()
+        {
+            overlay.SetActive(false);
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                overlay.SetActive(!overlay.activeSelf);
+            }
+        }
+    }
+}

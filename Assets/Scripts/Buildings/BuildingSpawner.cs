01389c34229e2fa21e631d892568f5e434d0c191
BuildTools <unconfigured@null.spigotmc.org>
2023-03-25 00:33:32 +0100

    dsdsgf

--- Assets/Scripts/Buildings/BuildingSpawner.cs
+++ Assets/Scripts/Buildings/BuildingSpawner.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using UI;
 using UnityEngine;
@@ -15,6 +14,7 @@ namespace Buildings
 
         private const int LayerIndex = 6;
         private Vector3 spawnPoint;
+        private BuildingPlatform selectedPlatform;
         private bool occupied;
 
         private void Start()
@@ -33,6 +33,11 @@ namespace Buildings
 
         private void Update()
         {
+            HandlePlayerInput();
+        }
+
+        private void HandlePlayerInput()
+        {
             if (Input.GetMouseButtonDown(0))
             {
                 Ray ray = _playerCamera.ScreenPointToRay(Input.mousePosition);
@@ -40,8 +45,16 @@ namespace Buildings
 
                 if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << LayerIndex))
                 {
-                    _buildingMenuController.Show();
-                    spawnPoint = hit.transform.position;
+                    selectedPlatform = hit.transform.GetComponent<BuildingPlatform>();
+                    if (!selectedPlatform.IsOccupied)
+                    {
+                        _buildingMenuController.Show();
+                        spawnPoint = hit.transform.position;
+                    }
+                    else
+                    {
+                        Debug.Log("Is occupied");
+                    }
                 }
             }
         }
@@ -54,7 +67,7 @@ namespace Buildings
 
         private void SpawnBuilding(BuildingType buildingType, Vector3 position)
         {
-            var buildingDefinition = _buildingDefinitions[(int)buildingType];
+            var buildingDefinition = _buildingDefinitions[(int) buildingType];
             var buildingPrefab = buildingDefinition._prefab;
 
             var building = Instantiate(buildingPrefab, position,
@@ -65,6 +78,8 @@ namespace Buildings
             buildingData._currentHp = buildingDefinition._stats._maxHp;
 
             _buildingContainer.AddActiveBuilding(buildingDefinition, buildingData);
+            
+            selectedPlatform.IsOccupied = true;
         }
     }
 }
